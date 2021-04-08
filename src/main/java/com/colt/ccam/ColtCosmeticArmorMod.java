78ef3d9acb272f7f54db761da547671f80ad8c29
GiraffeSummer <gidoselten@hotmail.com>
2021-04-08 14:03:58 +0200

    added curios (not rendering)

--- src/main/java/com/colt/ccam/ColtCosmeticArmorMod.java
+++ src/main/java/com/colt/ccam/ColtCosmeticArmorMod.java
@@ -1,28 +1,29 @@
 package com.colt.ccam;
 
-import com.colt.ccam.client.ClientRefrence;
-import com.colt.ccam.data.SingleItemRecipeProvider;
-import com.colt.ccam.registries.ccamBlocks;
-import com.colt.ccam.registries.ccamContainer;
-import com.colt.ccam.registries.ccamItems;
-import com.colt.ccam.registries.ccamRecipe;
-import com.colt.ccam.server.dedicated.DedicatedServerReference;
-import com.colt.ccam.sewingstation.SewingRecipe;
-import com.colt.ccam.sewingstation.SewingStationContainer;
-import com.colt.ccam.sewingstation.SewingStationScreen;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
-import net.minecraft.client.gui.ScreenManager;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.RenderTypeLookup;
-import net.minecraft.data.DataGenerator;
+import com.colt.ccam.client.ClientRefrence;
+import com.colt.ccam.registries.ccamItems;
+import com.colt.ccam.server.dedicated.DedicatedServerReference;
+import com.colt.ccam.sewingstation.SewingStationContainer;
+import com.colt.ccam.util.CuriosUtil;
+
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.container.ContainerType;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import top.theillusivec4.curios.Curios;
+import top.theillusivec4.curios.api.SlotTypeMessage;
+import top.theillusivec4.curios.api.SlotTypePreset;
 
 @Mod(ColtCosmeticArmorMod.MOD_ID)
 public class ColtCosmeticArmorMod {
@@ -37,31 +38,52 @@ public class ColtCosmeticArmorMod {
                 forgeEventBus = MinecraftForge.EVENT_BUS;
 
         SIDED_SYSTEM.setup(modEventBus, forgeEventBus);
+        
         addRegistries(modEventBus);
     }
 
     private void addRegistries(final IEventBus modEventBus) {
         ccamItems.ITEMS.register(modEventBus);
-        //ccamBlocks.BLOCKS.register(modEventBus);
-        //ccamContainer.CONTAINERS.register(modEventBus);
-        //ccamRecipe.RECIPES.register(modEventBus);
-        //modEventBus.addListener(this::onInitializeClient);
-       //modEventBus.addListener(this::gatherData);
+        modEventBus.addListener(this::enqueueIMC);
+        // ccamBlocks.BLOCKS.register(modEventBus);
+        // ccamContainer.CONTAINERS.register(modEventBus);
+        // ccamRecipe.RECIPES.register(modEventBus);
+        // modEventBus.addListener(this::onInitializeClient);
+        // modEventBus.addListener(this::gatherData);
     }
-/*
-    private void gatherData(final GatherDataEvent event) {
-		DataGenerator gen = event.getGenerator();
-		
-		if(event.includeServer()) {
-			gen.addProvider(new SingleItemRecipeProvider(gen));
-		}
-	}
 
-    private void onInitializeClient(FMLClientSetupEvent e) {
-        ScreenManager.registerFactory(SewingStationContainer, SewingStationScreen::new);
-
-        RenderTypeLookup.setRenderLayer(ccamBlocks.SEWING_STATION.get(), RenderType.getCutout());
-
+    @SubscribeEvent
+    public void enqueueIMC(final InterModEnqueueEvent event) {
+        SlotTypePreset[] slots = { SlotTypePreset.HEAD, SlotTypePreset.NECKLACE, SlotTypePreset.BACK,
+                SlotTypePreset.BODY, SlotTypePreset.HANDS, SlotTypePreset.RING, SlotTypePreset.CHARM };
+        List<SlotTypeMessage.Builder> builders = new ArrayList<>();
+        for (SlotTypePreset slot : slots) {
+            SlotTypeMessage.Builder builder = slot.getMessageBuilder();
+            if (slot == SlotTypePreset.RING) {
+                builder.size(2);
+            }
+            builder.cosmetic();
+            builders.add(builder);
+        }
+        for (SlotTypeMessage.Builder builder : builders) {
+            SlotTypeMessage message = builder.build();
+            InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, () -> message);
+        }
     }
-    */
+    /*
+     * private void gatherData(final GatherDataEvent event) { DataGenerator gen =
+     * event.getGenerator();
+     * 
+     * if(event.includeServer()) { gen.addProvider(new
+     * SingleItemRecipeProvider(gen)); } }
+     * 
+     * private void onInitializeClient(FMLClientSetupEvent e) {
+     * ScreenManager.registerFactory(SewingStationContainer,
+     * SewingStationScreen::new);
+     * 
+     * RenderTypeLookup.setRenderLayer(ccamBlocks.SEWING_STATION.get(),
+     * RenderType.getCutout());
+     * 
+     * }
+     */
 }
