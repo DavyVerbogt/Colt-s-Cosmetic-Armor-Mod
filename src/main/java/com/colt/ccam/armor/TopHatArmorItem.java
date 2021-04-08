e3beeffc8eb06658c10d056860296294311834cb
Davy Verbogt <36187331+DavyVerbogt@users.noreply.github.com>
2021-04-08 18:13:38 +0200

    Top Hat Curio Works Test

--- src/main/java/com/colt/ccam/armor/TopHatArmorItem.java
+++ src/main/java/com/colt/ccam/armor/TopHatArmorItem.java
@@ -1,28 +1,50 @@
 package com.colt.ccam.armor;
 
 import com.colt.ccam.ColtCosmeticArmorMod;
+import com.colt.ccam.client.render.entity.model.TopHatModel;
+import com.colt.ccam.curio.CurioArmorItem;
 
 import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.item.ArmorItem;
 import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.IDyeableArmorItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class TopHatArmorItem extends ArmorItem implements IDyeableArmorItem{
+public class TopHatArmorItem extends CurioArmorItem implements IDyeableArmorItem {
+
+    private static final ResourceLocation TEXTURE_OVERLAY = new ResourceLocation(ColtCosmeticArmorMod.MOD_ID,
+            "textures/models/armor/tophat_layer_1_overlay.png");
+    private static final ResourceLocation TEXTURE = new ResourceLocation(ColtCosmeticArmorMod.MOD_ID,
+            "textures/models/armor/tophat_layer_1.png");
 
     public TopHatArmorItem(IArmorMaterial materialIn, EquipmentSlotType slot, Properties builderIn) {
         super(materialIn, slot, builderIn);
     }
+
     @Override
-	public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
-		return ColtCosmeticArmorMod.SIDED_SYSTEM.getTopHatArmorModel(armorSlot);
-	}
-    
+    public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack,
+            EquipmentSlotType armorSlot, A _default) {
+        return ColtCosmeticArmorMod.SIDED_SYSTEM.getTopHatArmorModel(armorSlot);
+    }
+
     public int getColor(ItemStack stack) {
         CompoundNBT lvt_2_1_ = stack.getChildTag("display");
         return lvt_2_1_ != null && lvt_2_1_.contains("color", 99) ? lvt_2_1_.getInt("color") : 0X9B2D2A;
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    protected BipedModel<LivingEntity> createModel() {
+        return new TopHatModel(1f);
+    }
+
+    @Override
+    protected ResourceLocation getTexture() {
+        return TEXTURE_OVERLAY;
     }
 }
---
+++ src/main/java/com/colt/ccam/curio/CurioItem.java
@@ -0,0 +1,92 @@
+package com.colt.ccam.curio;
+
+
+import com.colt.ccam.registries.ccamItems;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Rarity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.EventPriority;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurio;
+import top.theillusivec4.curios.api.type.capability.ICurioItem;
+
+import javax.annotation.Nullable;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public abstract class CurioItem extends ccamItems implements ICurioItem {
+
+    private Object model;
+
+    protected boolean isEquippedBy(@Nullable LivingEntity entity) {
+        return entity != null && CuriosApi.getCuriosHelper().findEquippedCurio(this, entity).isPresent();
+    }
+
+    protected <T extends Event> void addListener(EventPriority priority, Class<T> eventClass, Consumer<T> listener, Function<T, LivingEntity> livingEntitySupplier) {
+        MinecraftForge.EVENT_BUS.addListener(priority, true, eventClass, event -> {
+            if (isEquippedBy(livingEntitySupplier.apply(event))) {
+                listener.accept(event);
+            }
+        });
+    }
+
+    protected <T extends Event> void addListener(Class<T> eventClass, Consumer<T> listener, Function<T, LivingEntity> livingEntitySupplier) {
+        addListener(EventPriority.NORMAL, eventClass, listener, livingEntitySupplier);
+    }
+
+    protected <T extends LivingEvent> void addListener(EventPriority priority, Class<T> eventClass, Consumer<T> listener) {
+        addListener(priority, eventClass, listener, LivingEvent::getEntityLiving);
+    }
+
+    protected <T extends LivingEvent> void addListener(Class<T> eventClass, Consumer<T> listener) {
+        addListener(EventPriority.NORMAL, eventClass, listener);
+    }
+
+    @Override
+    public boolean canEquipFromUse(SlotContext slotContext, ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public boolean canRender(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public void render(String identifier, int index, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, ItemStack stack) {
+        BipedModel<LivingEntity> model = getModel();
+        model.setupAnim(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+        model.prepareMobModel(entity, limbSwing, limbSwingAmount, partialTicks);
+        ICurio.RenderHelper.followBodyRotations(entity, model);
+        IVertexBuilder vertexBuilder = ItemRenderer.getFoilBuffer(renderTypeBuffer, model.renderType(getTexture()), false, stack.hasFoil());
+        model.renderToBuffer(matrixStack, vertexBuilder, light, OverlayTexture.NO_OVERLAY, 1, 1, 1, 1);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    protected final BipedModel<LivingEntity> getModel() {
+        if (model == null) {
+            model = createModel();
+        }
+
+        //noinspection unchecked
+        return (BipedModel<LivingEntity>) model;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    protected abstract BipedModel<LivingEntity> createModel();
+
+    protected abstract ResourceLocation getTexture();
+}
