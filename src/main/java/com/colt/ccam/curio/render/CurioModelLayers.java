d3d855984223a833f52de5626e54bc35186d31f2
GiraffeSummer <gidoselten@hotmail.com>
2022-03-17 22:51:14 +0100

    fourth of july item texture

--- src/main/java/com/colt/ccam/curio/render/CurioModelLayers.java
+++ src/main/java/com/colt/ccam/curio/render/CurioModelLayers.java
@@ -12,19 +12,17 @@ import net.minecraftforge.client.event.EntityRenderersEvent;
 import java.util.function.Supplier;
 
 public class CurioModelLayers {
-    public static final ModelLayerLocation
-    CAT_EARS = createLayerLocation("catears_curio");
+    public static final ModelLayerLocation CAT_EARS = createLayerLocation("catears_curio");
 
     private static Supplier<LayerDefinition> layer(MeshDefinition mesh, int textureWidth, int textureHeight) {
         return () -> LayerDefinition.create(mesh, textureWidth, textureHeight);
     }
 
-        public static void register(EntityRenderersEvent.RegisterLayerDefinitions event) {
-        event.registerLayerDefinition(CAT_EARS, () -> CatEarCurioModel.createCatEars(new CubeDeformation(0.5f)));
+    public static void register(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        //event.registerLayerDefinition(CAT_EARS, () -> CatEarCurioModel.createCatEars(new CubeDeformation(0.5f)));
     }
 
     public static ModelLayerLocation createLayerLocation(String name) {
         return new ModelLayerLocation(new ResourceLocation(ColtCosmeticArmorMod.MOD_ID, name), name);
     }
 }
-
